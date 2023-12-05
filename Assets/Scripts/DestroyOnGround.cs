8cb4fb80a0f1c5a9a09b4f2d1d0558b7a87348e0
HienaDev <ant.rodrigues1999@gmail.com>
2023-12-05 15:30:33 +0000

    Fix ball not registering sometimes

--- Assets/Scripts/DestroyOnGround.cs
+++ Assets/Scripts/DestroyOnGround.cs
@@ -10,16 +10,26 @@ public class DestroyOnGround : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         // Convert the collision object layer
         int x = 1 << collision.gameObject.layer;
 
         // If we contact with the layer we update the player's position and destroy the ball
         if (x == floorLayer.value)
-        { 
+        {
 
-        GetComponentInParent<PrefabManager>().UpdatePlayerPosition(transform.position);
+            GetComponentInParent<PrefabManager>().UpdatePlayerPosition(transform.position);
 
-        Destroy(gameObject);
+            Destroy(gameObject.transform.parent.gameObject);
         }
     }
 }
